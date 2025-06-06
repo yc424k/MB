34f13fe4b3a2f6d812f884bf45dd71891750a0e0
You_Blue <kyc0424@sch.ac.kr>
2025-06-06 19:52:02 +0900

    .

--- launch/md_robot_node_use_MDUI.launch.py
+++ launch/md_robot_node_use_MDUI.launch.py
@@ -1,16 +1,39 @@
-<launch>
-    <node pkg="md" type="md_robot_node" name="md_robot_node" output="screen">
-        <param name = "use_MDUI"                 value = "1"/>      <!-- 0: not use MDUI, 1: use MDUI -->
-        <param name = "serial_port"              value = "ttyUSB1"/> <!-- COM port -->
-        <param name = "serial_baudrate"          value = "57600"/>   <!-- MDUI baudrate -->
-        <param name = "wheel_radius"             value = "0.0935"/> <!-- unit: meter -->
-        <param name = "wheel_length"             value = "0.454"/>  <!-- unit: meter -->
-        <param name = "reduction"                value = "30"/>
-        <param name = "reverse_direction"        value = "0"/>      <!-- 0: forward, 1: reverse -->
-        <param name = "maxrpm"                   value = "1000"/>   <!-- unit: RPM -->
-        <param name = "enable_encoder"           value = "0"/>      <!-- 0: hall sensor, 1: encoder, Use the encoder only when using an in-wheel motor -->
-        <param name = "encoder_PPR"              value = "900"/>    <!-- if use encoder position, encoder PPR, reference PID 126(PID_PPR) -->
-        <param name = "slow_start"               value = "300"/>    <!-- unit: RPM -->
-        <param name = "slow_down"                value = "300"/>    <!-- unit: RPM -->
-    </node>
-</launch>
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    # 패키지 이름
+    pkg_name = 'md'
+
+    # 실행할 노드와 파라미터를 정의합니다.
+    md_robot_node = Node(
+        package=pkg_name,
+        executable='md_robot_node',
+        name='md_robot_node',
+        output='screen',
+        emulate_tty=True, # ROS1의 output="screen"과 유사한 동작을 위해 추가
+        parameters=[
+            {'use_MDUI': 0},
+            {'serial_port': 'ttyUSB0'},
+            {'serial_baudrate': 57600},
+            {'wheel_radius': 0.0935},
+            {'wheel_length': 0.454},
+            {'reduction': 30.0},
+            {'reverse_direction': 0},
+            {'maxrpm': 1000},
+            {'enable_encoder': 0},
+            {'encoder_PPR': 900},
+            {'slow_start': 300},
+            {'slow_down': 300},
+        ]
+    )
+
+    # LaunchDescription 객체를 생성하고 노드를 추가합니다.
+    ld = LaunchDescription()
+    ld.add_action(md_robot_node)
+    
+    return ld
